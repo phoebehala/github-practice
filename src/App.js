a540568c3805dcf55d8e8a8bab7be8b943aefb98
Lada496 <67321065+Lada496@users.noreply.github.com>
2022-02-09 11:42:13 -0800

    delete greetings

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2>My name is Lada</h2>
-      <h1>Hello world</h1>
       <h1>Hello Yuko</h1>
       <h1>Hello Lily</h1>
       <h1>Hello Ayako</h1>
